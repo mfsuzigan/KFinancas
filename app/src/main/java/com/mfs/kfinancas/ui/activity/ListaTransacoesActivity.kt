cab91f7c59570df4d87cffbd35ca5c9190328757
Michel Suzigan <mfsuzigan@gmail.com>
2018-08-20 08:06:46 -0300

    Evolucao do resumo de transacoes

--- app/src/main/java/com/mfs/kfinancas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/mfs/kfinancas/ui/activity/ListaTransacoesActivity.kt
@@ -3,13 +3,11 @@ package com.mfs.kfinancas.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mfs.kfinancas.R
-import com.mfs.kfinancas.extension.formataParaBrasileiro
 import com.mfs.kfinancas.model.Transacao
 import com.mfs.kfinancas.model.enum.TipoTransacao
 import com.mfs.kfinancas.ui.adapter.ListaTransacoesAdapter
 import com.mfs.kfinancas.ui.view.ResumoView
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
-import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 
 class ListaTransacoesActivity : AppCompatActivity() {
@@ -29,7 +27,7 @@ class ListaTransacoesActivity : AppCompatActivity() {
     }
 
     private fun configurarTotalizadores(transacoes:List<Transacao>){
-        ResumoView(window.decorView, transacoes).configurar()
+        ResumoView(window.decorView, this, transacoes).configurar()
     }
 
     private fun obterTransacoesExemplo(): List<Transacao> {
--- app/src/main/java/com/mfs/kfinancas/ui/view/ResumoView.kt
+++ app/src/main/java/com/mfs/kfinancas/ui/view/ResumoView.kt
@@ -1,19 +1,39 @@
 package com.mfs.kfinancas.ui.view
 
+import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.View
+import com.mfs.kfinancas.R
 import com.mfs.kfinancas.extension.formataParaBrasileiro
 import com.mfs.kfinancas.model.Resumo
 import com.mfs.kfinancas.model.Transacao
 import kotlinx.android.synthetic.main.resumo_card.view.*
 
-class ResumoView(private val view:View, transacoes:List<Transacao>) {
+class ResumoView(private val view:View, context:Context, transacoes:List<Transacao>) {
 
     private val resumo = Resumo(transacoes)
+    private val corReceita = ContextCompat.getColor(context, R.color.receita)
+    private val corDespesa = ContextCompat.getColor(context, R.color.despesa)
 
     fun configurar(){
-        view.resumo_card_receita.text = resumo.receita.formataParaBrasileiro()
         view.resumo_card_despesa.text = resumo.despesa.formataParaBrasileiro()
         view.resumo_card_total.text = resumo.total.formataParaBrasileiro()
+    }
+
+    private fun configurarReceita(){
+        view.resumo_card_receita.text = resumo.receita.formataParaBrasileiro()
+        view.resumo_card_receita.setTextColor(corReceita)
+    }
+
+    private fun configurarDespesa(){
+        view.resumo_card_despesa.text = resumo.despesa.formataParaBrasileiro()
+        view.resumo_card_despesa.setTextColor(corDespesa)
+    }
+
+    private fun configurarTotal(){
+        view.resumo_card_total.text = resumo.total.formataParaBrasileiro()
+        view.resumo_card_total.setTextColor(
+                        if (resumo.total.signum() > 0) corReceita else corDespesa)
     }
 }
 
